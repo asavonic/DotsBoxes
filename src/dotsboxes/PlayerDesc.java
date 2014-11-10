baac2161aa82c4a24a706665bf0d13d2fe225895
Andrew Savonichev <savonichev.andr@gmail.com>
2014-11-11 01:54:50 +0300

    add PlayerDesc implementation

--- src/dotsboxes/PlayerDesc.java
+++ src/dotsboxes/PlayerDesc.java
@@ -3,10 +3,33 @@
  */
 package dotsboxes;
 
+import java.net.InetAddress;
+
 /**
- *
- *
+ * Represents the player.
+ * Contains his address, name and unique hash
  */
 public class PlayerDesc {
-
+	InetAddress m_InetAdress;
+	String m_Name;
+	byte[] m_Hash;
+	
+	public InetAddress getInetAdress() {
+		return m_InetAdress;
+	}
+	public void setInetAdress(InetAddress inetAdress) {
+		m_InetAdress = inetAdress;
+	}
+	public String getName() {
+		return m_Name;
+	}
+	public void setName(String name) {
+		m_Name = name;
+	}
+	public byte[] getHash() {
+		return m_Hash;
+	}
+	public void setHash(byte[] hash) {
+		m_Hash = hash;
+	}
 }
