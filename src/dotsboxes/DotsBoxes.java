87a1b97445bbcfd81fd5eb40fabd35b75dfa58bf
vlad <vladromanovforjob@gmail.com>
2014-11-11 04:34:26 +0400

    Add package name.

--- src/dotsboxes/DotsBoxes.java
+++ src/dotsboxes/DotsBoxes.java
@@ -1,10 +1,12 @@
+package dotsboxes;
 
 public class DotsBoxes {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World");
 	}
 
