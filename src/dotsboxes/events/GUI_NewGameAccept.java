534912fafd0997b81633c26029bba38ca5a4cb49
vlad <vladromanovforjob@gmail.com>
2014-12-06 19:04:17 +0300

    Added arg to constructor GUI_NewGameAccept.

--- src/dotsboxes/events/GUI_NewGameAccept.java
+++ src/dotsboxes/events/GUI_NewGameAccept.java
@@ -6,10 +6,12 @@ import dotsboxes.players.PlayerDesc;
 public class GUI_NewGameAccept extends Event
 {
 	RemoteNewGameRequest m_game_request;
-	public GUI_NewGameAccept( RemoteNewGameRequest game_request) 
+	int m_num_local_players;
+	public GUI_NewGameAccept( RemoteNewGameRequest game_request, int num_local_players) 
 	{
 		super(EventType.GUI_New_Game_Accept);
 		m_game_request = game_request;
+		m_num_local_players = num_local_players;
 	}
 	
 	public PlayerDesc getSender()
@@ -24,6 +26,6 @@ public class GUI_NewGameAccept extends Event
 	
 	public int getNumLocalPlayers()
 	{
-		return m_game_request.getNumLocalPlayers();
+		return m_num_local_players;
 	}
 }
