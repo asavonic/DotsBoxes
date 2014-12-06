0fdbf6665e36864b32501e0bdf99c0015df76140
vlad <vladromanovforjob@gmail.com>
2014-12-07 02:42:36 +0300

    Added TaggedValue implements Serializable.

--- src/dotsboxes/utils/TaggedValue.java
+++ src/dotsboxes/utils/TaggedValue.java
@@ -1,6 +1,12 @@
 package dotsboxes.utils;
 
-public class TaggedValue<T, Tag> {
+import java.io.Serializable;
+
+public class TaggedValue<T, Tag> implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2801504424267824887L;
 	public TaggedValue(T _value, Tag _tag) {
 		value = _value;
 		tag = _tag;
