3d6df088d42ecb03bf97a1366cfe2d36f92d0292
vlad <vladromanovforjob@gmail.com>
2014-12-08 15:28:34 +0300

    Revert "add FixedPortRMISocketFactory to allow RMI connection behind NAT"
    This reverts commit 42843544fc2824068659b69022b2dc732ffcbd31.

--- src/dotsboxes/DotsBoxes.java
+++ src/dotsboxes/DotsBoxes.java
@@ -1,16 +1,13 @@
 package dotsboxes;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.server.RMISocketFactory;
 
 import dotsboxes.players.PlayerDesc;
 import dotsboxes.rmi.ConnectionManager;
-import dotsboxes.rmi.FixedPortRMISocketFactory;
 import dotsboxes.rmi.exceptions.ConnectionAlreadyEstablished;
 import dotsboxes.utils.Configuration;
 import dotsboxes.utils.Debug;
@@ -22,13 +19,6 @@ public class DotsBoxes {
 	 */
 	public static void main(String[] args) 
 	{
-		
-		try {
-			RMISocketFactory.setSocketFactory(new FixedPortRMISocketFactory());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
 		try {
 			System.out.println("Hello World");
 			int port = Integer.parseInt(args[0]);
